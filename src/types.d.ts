1cfbf3d69347c0f04e2c40a084b783ab7588e933
foo <5144598+bar@users.noreply.github.com>
2018-10-18 12:42:43 +0300

    refactor: remove temporary types

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,8 +1,2 @@
 declare module '*.vert'
 declare module '*.frag'
-
-interface CanvasRenderingContext2D {
-  filter: string
-  imageSmoothingEnabled: boolean
-  imageSmoothingQuality: 'low' | 'medium' | 'high'
-}
