3efd7d19644506517175e5b908095d464d331612
- <5144598+-@users.noreply.github.com>
2020-01-11 08:24:43 +0300

    demo: refactor

--- demo/lib/util.js
+++ demo/lib/util.js
@@ -1,14 +1,18 @@
 export const frame = () =>
   new Promise(requestAnimationFrame)
 
-export const update = (target, src) =>
-  Object.entries(src)
-    .filter(([k, v]) => target[k] !== v)
-    .forEach(([k, v]) => target[k] = v)
-
 export const fetchImage = opts => new Promise((ok, err) => {
   Object.assign(new Image, opts, {
     onload() { ok(this) },
     onerror: err
   })
 })
+
+export const context2d = () => {
+  const canvas = new OffscreenCanvas(0, 0)
+  const context = canvas.getContext('2d')
+  return (width, height) => {
+    Object.assign(canvas, { width, height })
+    return context
+  }
+}
